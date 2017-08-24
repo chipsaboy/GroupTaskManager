99dc727014b2db818b51b891c4fbe36731a1790d
Nathan Campbell <nathancampbell@outlook.com>
2017-08-24 16:42:37 -0500

    added tag resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :tags
   resources :tasks
   resources :lists
   devise_for :users
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170824213324) do
+ActiveRecord::Schema.define(version: 20170824214118) do
 
   create_table "lists", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -22,6 +28,13 @@ ActiveRecord::Schema.define(version: 20170824213324) do
     t.string "name"
     t.text "description"
     t.integer "state", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_lists", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "list_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
