61ceffb86ae9715a9d9c5e20c6eb7e01353e2110
Nathan Campbell <nathancampbell@outlook.com>
2017-08-24 18:39:05 -0500

    built out part of list controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,27 @@
 class ListsController < ApplicationController
+
+  def index
+    @lists = List.all
+  end
+
+  def new
+    @list = List.new
+    @list.tasks.build
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to @list
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name, tasks_attributes: [:name, :description, :state, :due_date])
+  end
+
 end
