0a7228ac088298dc07198be1b5d64e6e8e5b5591
Nathan Campbell <nathancampbell@outlook.com>
2017-08-24 18:10:51 -0500

    working on activerecord associations

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,14 @@
 class Task < ApplicationRecord
+  has_many :task_tags
+  has_many :tags, through: :task_tags
+  has_many :user_tasks
+  has_many :users, through: :user_tasks
+  accepts_nested_attributes_for :tags
+
+  def tags_attributes=(tag_attributes)
+    tag_attributes.values.each do |tag_attribute|
+      tag = Tag.find_or_create_by(tag_attribute)
+      self.tags << tag
+    end
+  end
 end
