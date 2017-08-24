a63b317ae1d48ec51b7aba82bb62f3bdfbc7c637
Nathan Campbell <nathancampbell@outlook.com>
2017-08-24 17:17:42 -0500

    setting up activerecord validations for list and user, added devise omniauth method to users

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,6 @@
 class List < ApplicationRecord
+	has_many :tasks
+	has_many :user_lists
+	has_many :users, through: :user_lists
+	accept_nested_attributes_for :tasks
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,19 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :user_lists
+  has_many :user_tasks
+  has_many :lists, through: :user_lists
+  has_many :tasks, through: :user_tasks
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+    devise :omniauthable, :omniauth_providers => [:facebook, :google_oauth2, :twitter]
+
+  def self.from_omniauth(auth)
+	where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+	  user.email = auth.info.email
+	  user.password = Devise.friendly_token[0,20]
+	  user.name = auth.info.name
+    end
+  end
 end
