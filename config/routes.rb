af84a0e86922e7b64cad8ce1b80c0edd3a7d325e
Nathan Campbell <nathancampbell@outlook.com>
2017-08-24 22:55:01 -0500

    simplified omniauth to just facebook

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :tags
   resources :tasks
   resources :lists
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 end
