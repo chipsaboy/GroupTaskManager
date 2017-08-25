a8ff05a2afe7f153f13e57ed95fc5ba8a2d9ab31
Nathan Campbell <nathancampbell@outlook.com>
2017-08-25 13:46:22 -0500

    added task controller params and index, added nested routes

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,12 @@
 class TasksController < ApplicationController
+  def index
+    @task = Task.all
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description, :state, :due_date, :list_id, user_ids:[], user_attributes: [:name], tag_ids:[], tags_attributes: [:name])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,12 @@ Rails.application.routes.draw do
   resources :lists do
     resources :tasks
   end
+  resources :tasks do
+  	resources :tags
+  end
+  resources :tasks do
+  	resources :users
+  end
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
