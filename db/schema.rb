b36ed1555e71ef677203c1526465e9bf93cdd2b7
Nathan Campbell <nathancampbell@outlook.com>
2017-08-26 15:12:46 -0500

    added foreign key to tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170824221837) do
+ActiveRecord::Schema.define(version: 20170826200451) do
 
   create_table "lists", force: :cascade do |t|
     t.string "name"
@@ -32,6 +32,8 @@ ActiveRecord::Schema.define(version: 20170824221837) do
     t.datetime "due_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "list_id"
+    t.index ["list_id"], name: "index_tasks_on_list_id"
   end
 
   create_table "user_tasks", force: :cascade do |t|
