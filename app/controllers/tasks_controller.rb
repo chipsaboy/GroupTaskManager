33607d0d50309278d1df75f74597bacc6b6fb460
Nathan Campbell <nathancampbell@outlook.com>
2017-08-26 15:39:41 -0500

    added update to tasks controller and find_list method

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,6 @@
 class TasksController < ApplicationController
 
   def create
-    @list = List.find(params[:list_id])
     @task = @list.tasks.build(task_params)
     if @task.save
       redirect_to list_path(@list)
@@ -10,7 +9,21 @@ class TasksController < ApplicationController
     end
   end
 
+  def update
+    @task = @list.tasks.find(params[:id])
+    @task.update(task_params)
+    if @task.save
+      redirect_to list_path(@list)
+    else
+      render @list, alert: @task.errors.full_messages
+    end
+  end
+
   private
+
+  def find_list
+    @list = List.find(params[:list_id])
+  end
 
   def task_params
     params.require(:task).permit(:name, :state, :due_date, :list_id, user_ids:[], tag_ids:[])
