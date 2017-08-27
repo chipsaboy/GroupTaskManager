0b6561d8f4a377866b1d2a28a6edbf8b47d51c63
Nathan Campbell <nathancampbell@outlook.com>
2017-08-26 23:55:04 -0500

    removed materialize css due to issue with datetime picker, added semantic UI

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require jquery
-//= require materialize-sprockets
+//= require semantic-ui
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
