5ce2b1db3987a1bc96bfb8d39de17c5a4d8802d1
Nathan Campbell <nathancampbell@outlook.com>
2017-08-26 23:56:41 -0500

    added home controller and changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :lists, except: :new do
-    resources :tasks, only: [:create, :update, :destroy]
+  resources :lists do
+    resources :tasks
   end
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   devise_scope :user do
