752d5bc08131abf5e648d91b674debbd4a7dddfe
Nathan Campbell <nathancampbell@outlook.com>
2017-08-29 17:02:33 -0500

    added user_id to list

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170827044030) do
+ActiveRecord::Schema.define(version: 20170829211403) do
 
   create_table "lists", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_lists_on_user_id"
   end
 
   create_table "tags", force: :cascade do |t|
