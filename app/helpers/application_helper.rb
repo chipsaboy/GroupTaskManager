64b94f6b87fd290951390a0a5df441fa7a688675
Nathan Campbell <nathancampbell@outlook.com>
2017-08-29 13:55:02 -0500

    changed time format in datetime select and added a helpers to display datetime in mm dd yyyy format and a countdown to that date

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def display_date(date)
+    return date.strftime("%A %B %e %Y at %l:%m%p")
+  end
 end
--- app/helpers/lists_helper.rb
+++ app/helpers/lists_helper.rb
@@ -1,2 +1,5 @@
 module ListsHelper
+  def time_remaining(date)
+    distance_of_time_in_words(date, DateTime.now)
+  end
 end
