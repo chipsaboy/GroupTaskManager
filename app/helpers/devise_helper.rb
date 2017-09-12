439dd300aa1fddf3660f2afc42db1c5f5aaeab47
Nathan Campbell <nathancampbell@outlook.com>
2017-09-12 17:08:23 -0500

    added helpers to task and devise

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -12,4 +12,8 @@ module DeviseHelper
 
     html.html_safe
   end
+
+  def facebooked
+    current_user.provider == "facebook"
+  end
 end
--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -2,4 +2,8 @@ module TasksHelper
 	def pretty_date(date)
 		date.strftime("%A, %B %d, %Y at %l:%M%P ")
 	end
+
+	def allowed_complete?(task)
+		task.users.exists?(name: current_user.name)
+	end
 end
