082d0d11fe673fa55a92fdf4191a00c0b7fb4450
Nathan Campbell <nathancampbell@outlook.com>
2017-09-12 18:32:04 -0500

    added exception method if user denies email with facebook login and removed email from profile for facebook users

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,22 @@ class User < ApplicationRecord
   has_many :lists, dependent: :destroy
   has_many :tasks, through: :user_tasks, dependent: :destroy
 
+  private
+
   def self.from_omniauth(auth)
 	where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
     user.provider = auth.provider
-	  user.email = auth.info.email
-	  user.password = Devise.friendly_token[0,20]
-	  user.name = "#{user.email[/^[^@]*/]}#{rand(1 .. 500)}"
+    if auth.info.email
+	    user.email = auth.info.email
+      user.name = "#{user.email[/^[^@]*/]}#{rand(1 .. 500)}"
+    else
+      user.name = "user#{rand(1 .. 999999)}"
     end
+	  user.password = Devise.friendly_token[0,20]
+    end
+  end
+
+  def email_required?
+    super && provider.blank?
   end
 end
