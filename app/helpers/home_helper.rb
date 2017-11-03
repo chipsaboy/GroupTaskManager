80082eca6363ae2c6432a0abfd3b23f321ff6a26
Nathan Campbell <nathancampbell@outlook.com>
2017-11-03 10:51:13 -0500

    added helpers for tasks counts

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,15 @@
 module HomeHelper
+
+	def current_tasks_incomplete
+		current_user.tasks.incomplete.size
+	end
+
+	def current_tasks_complete
+		current_user.tasks.completed.size
+	end
+
+	def current_tasks_overdue
+		current_user.tasks.overdue_tasks.size
+	end
+
 end
