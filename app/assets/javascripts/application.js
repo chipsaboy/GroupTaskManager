2e8b6756737900755aaceb591ce2836e70873b7f
Nathan Campbell <nathancampbell@outlook.com>
2017-11-27 13:27:12 -0600

    fixed issue with order

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,16 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery3
 //= require popper
-//= require bootstrap-sprockets
-//= require jquery_ujs
 //= require turbolinks
 //= require flatpickr
+//= require strftime-min
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap-sprockets
 //= require_tree .
-
-$(document).ready(function(){
-    flatpickr('#flatpickr-input', {
-    	enableTime: true, altInput: true
-    });
-});
