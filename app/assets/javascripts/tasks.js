4e73c0735a934cfe8820b9346ca3cdc186782db2
Nathan Campbell <nathancampbell@outlook.com>
2017-11-28 21:36:54 -0600

    added renderTask prototype

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -27,6 +27,26 @@ Task.success = function(json) {
   $("div.ui.relaxed.divided.items").append(taskCard)
 }
 
+Task.prototype.renderTask = function() {
+  return `
+  <div class="item db card" id="task-${this.id}">
+    <h4 id="task-color">
+      <a href="/lists/${this.list_id}/tasks/${this.id}/edit">Test</a>
+    </h4>
+    <p>Due Date: ${this.due_date}
+    </p>
+      <p>Assigned to:
+          ${this.users}
+    </p>
+    <p id="task-color">
+        <span class="badge badge-info">${this.tags}</span>
+    </p>
+      <form class="button_to" method="post" action="/lists/${this.list_id}/tasks/${this.id}/complete"><input type="hidden" name="_method" value="put"><button class="btn btn-xs btn-login sidebs" data-confirm="Are you sure?" type="submit" value="Done"><
+      </form>
+  </div>
+  `
+}
+
 function newTaskListener = function() {
 	$("form#new_task").on('submit', Task.newTask)
 }
